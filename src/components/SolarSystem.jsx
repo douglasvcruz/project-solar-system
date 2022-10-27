c9d2145f3fd2d0ba31342c10d9df1ff4294f91ae
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-27 18:36:34 -0300

    2º Requisito

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+export default class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">SolarSystem</div>
+    );
+  }
+}
