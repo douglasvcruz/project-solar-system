5e9850889cf8283300c29ab88867d422674c2ded
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-28 01:18:25 -0300

    8º Requisito

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 import Missions from './components/Missions';
 
 class App extends Component {
@@ -10,7 +9,6 @@ class App extends Component {
       <div>
         <Header />
         <SolarSystem />
-        <Title headline="test" />
         <Missions />
       </div>
     );
